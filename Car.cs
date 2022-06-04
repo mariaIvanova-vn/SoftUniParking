ec60569374a9c76bdaa0ca26ce34f5a63272b5c8
User <User@DESKTOP-VDOVIB3>
2022-06-04 15:32:48 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int HorsePower { get { return horsePower; } set { horsePower = value; } }
+        public string RegistrationNumber { get { return registrationNumber; } set { registrationNumber = value; } }
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Make: {Make}\n");
+            sb.Append($"Model: {Model}\n");
+            sb.Append($"HorsePower: {HorsePower}\n");
+            sb.Append($"RegistrationNumber: {RegistrationNumber}");
+            return sb.ToString();
+        }
+    }
+}
